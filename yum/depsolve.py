e9721ad2574b1b0f9b8156d0c1d85c0b751755c3
James Antill <james@and.org>
2013-02-22 14:08:56 -0500

    Use setAsDep instead of mangling relatedto directly, for bz#905899.

--- yum/depsolve.py
+++ yum/depsolve.py
@@ -1095,7 +1095,7 @@ class Depsolve(object):
                     continue
                 for member in self.tsInfo.getMembersWithState(
                     pkgtup=po.pkgtup, output_states=TS_INSTALL_STATES):
-                    member.relatedto.append((txmbr.po, 'dependson'))
+                    member.setAsDep(txmbr.po)
 
         return ret
 

2fb171d13078c9219971f30f2ad3a718216ead9a
James Antill <james@and.org>
2013-04-24 12:04:13 -0400

    Skip var_arch storage in yumdb.

--- yum/__init__.py
+++ yum/__init__.py
@@ -1849,8 +1849,11 @@ much more problems).
                 po.yumdb_info.reason = txmbr.reason
                 po.yumdb_info.releasever = self.conf.yumvar['releasever']
                 for var in self.conf.yumvar: # Store all yum variables.
+                    # Skip some of the variables...
                     if var == 'releasever': continue
                     if var == 'basearch': continue # This "never" changes.
+                    if var == 'arch':     continue
+                    # Skip uuid?
                     setattr(po.yumdb_info, 'var_' + var, self.conf.yumvar[var])
                 if hasattr(self, 'args') and self.args:
                     po.yumdb_info.command_line = ' '.join(self.args)

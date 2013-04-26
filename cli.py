rpm-software-management/dnf

commit fc395ef14b135b3591488816ec902057fd8d03f4

Eliminate an unnecessary round of transaction re-population
Initialize + populate the transaction set to be used for the test-run
before calling rpm_check_debug to let them use the same set, avoiding
one round of the fairly expensive re-population of the entire transaction.

cli.py
@@ -504,6 +504,12 @@ class YumBaseCli(yum.YumBase, output.YumOutput):
         if self.gpgsigcheck(downloadpkgs) != 0:
             return 1
         
+        self.initActionTs()
+        # save our dsCallback out
+        dscb = self.dsCallback
+        self.dsCallback = None # dumb, dumb dumb dumb!
+        self.populateTs(keepold=0) # sigh
+
         if self.conf.rpm_check_debug:
             rcd_st = time.time()
             self.verbose_logger.log(yum.logginglevels.INFO_2, 
@@ -537,12 +543,6 @@ class YumBaseCli(yum.YumBase, output.YumOutput):
             
         
         testcb = RPMTransaction(self, test=True)
-        
-        self.initActionTs()
-        # save our dsCallback out
-        dscb = self.dsCallback
-        self.dsCallback = None # dumb, dumb dumb dumb!
-        self.populateTs(keepold=0) # sigh
         tserrors = self.ts.test(testcb)
         del testcb
 

yum/__init__.py
@@ -4804,6 +4804,12 @@ class YumBase(depsolve.Depsolve):
     
     def _doTestTransaction(self,callback,display=None):
         ''' Do the RPM test transaction '''
+        self.initActionTs()
+        # save our dsCallback out
+        dscb = self.dsCallback
+        self.dsCallback = None # dumb, dumb dumb dumb!
+        self.populateTs( keepold=0 ) # sigh
+
         # This can be overloaded by a subclass.    
         if self.conf.rpm_check_debug:
             self.verbose_logger.log(logginglevels.INFO_2, 
@@ -4833,14 +4839,7 @@ class YumBase(depsolve.Depsolve):
         # overwrite the default display class
         if display:
             testcb.display = display
-        # clean out the ts b/c we have to give it new paths to the rpms 
-        del self.ts
   
-        self.initActionTs()
-        # save our dsCallback out
-        dscb = self.dsCallback
-        self.dsCallback = None # dumb, dumb dumb dumb!
-        self.populateTs( keepold=0 ) # sigh
         tserrors = self.ts.test( testcb, conf=tsConf )
         del testcb
   
@@ -4870,10 +4869,6 @@ class YumBase(depsolve.Depsolve):
 
     def _run_rpm_check_debug(self):
         results = []
-        # save our dsCallback out
-        dscb = self.dsCallback
-        self.dsCallback = None # dumb, dumb dumb dumb!
-        self.populateTs(test=1)
         self.ts.check()
         for prob in self.ts.problems():
             #  Newer rpm (4.8.0+) has problem objects, older have just strings.
@@ -4881,7 +4876,6 @@ class YumBase(depsolve.Depsolve):
             # now just be compatible.
             results.append(to_str(prob))
 
-        self.dsCallback = dscb
         return results
 
     def add_enable_repo(self, repoid, baseurls=[], mirrorlist=None, **kwargs):

rpm-software-management/yum

commit 608420459abbce10e2ea0c8a08c93c89754a417a

Resurrect diskspacecheck=0 functionality
Commit fc395ef14b135b3591488816ec902057fd8d03f4 broke the option
to disable disk space checking, presumably because cli.py adds
the filter flag to tsInfo.probFilterFlags, not the transaction
set itself. With the intermediate transaction set creation now removed,
initActionTs() doesn't get called again so nothing looks at the
tsInfo.probFilterFlags at that point.  Moving it to initActionTs()
where other similar initialization is done makes it more consistent
and more importantly, resurrects the functionality.

cli.py
@@ -661,8 +661,6 @@ class YumBaseCli(yum.YumBase, output.YumOutput):
         tt_st = time.time()            
         self.verbose_logger.log(yum.logginglevels.INFO_2,
             _('Running transaction test'))
-        if not self.conf.diskspacecheck:
-            self.tsInfo.probFilterFlags.append(rpm.RPMPROB_FILTER_DISKSPACE)
             
         self.ts.order() # order the transaction
         self.ts.clean() # release memory not needed beyond this point

yum/depsolve.py
@@ -199,6 +199,9 @@ class Depsolve(object):
             else:
                 self.logger.critical(_('Invalid tsflag in config file: %s'), flag)
 
+        if not self.conf.diskspacecheck:
+            self._add_prob_flags(rpm.RPMPROB_FILTER_DISKSPACE)
+
         probfilter = 0
         for flag in self.tsInfo.probFilterFlags:
             probfilter |= flag